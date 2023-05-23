f9e08968c2710d7c9a0bf4eae8f8f024826249cb
Lysboi <110478866+theLysboi@users.noreply.github.com>
2023-05-23 18:11:02 +0300

    Add files via upload

---
+++ BetterBodies/modinfo.lua
@@ -0,0 +1,9 @@
+local modInfo = {
+  name = "Better Bodies",
+  description = "The Better Bodies mod, which makes character models more realistic and gives the game a more pleasant appearance!",
+  preview = "logo.png",
+  version = "0.4",
+  type = "app",
+  developer = "Lysboi"
+}
+return modInfo
